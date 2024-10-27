59619e0d67b37165eff7262a8d68638131f87c9f
Zane Jude <zjude04@gmail.com>
2024-10-27 14:39:38 -0500

    created ImageCaptureActivity

--- app/src/main/java/com/example/msa_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/msa_assignment2/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    //Test repo connect
     @Composable
     fun MainScreen() {
         Column(modifier = Modifier.padding(16.dp)) {
